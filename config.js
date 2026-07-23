cda61b6340ec21059386b3b0c288bbf7595bfea0
Monedita <qenterprise9@gmail.com>
2026-07-23 10:01:19 -0400

    Wire real Apps Script webhook URL into config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.PSL_CONFIG = {
     phoneDisplay: "(772) 247-8149",
     phoneTel: "+17722478149",
-    leadWebhookUrl: "",
+    leadWebhookUrl: "https://script.google.com/macros/s/AKfycbxD8nUyoblD2E51ez7OYBaRWjYWMeK7Nkj-re0EdxuYHTb5JSrgAs02JZS5O9ZqXJiRwQ/exec",
     ga4MeasurementId: "",
     metaPixelId: ""
 };
